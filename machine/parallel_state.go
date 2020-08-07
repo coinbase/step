4205f9cf5cad7b2eb0d5803d8d5ea1013ada9fbe
Michael Ushakov <um.nix.user@gmail.com>
2020-08-07 13:37:22 +0500

    ParallelState impl

--- machine/parallel_state.go
+++ machine/parallel_state.go
@@ -3,19 +3,46 @@ package machine
 import (
 	"context"
 	"fmt"
-
 	"github.com/coinbase/step/utils/to"
+	"sync"
 )
 
 type ParallelState struct {
 	stateStr // Include Defaults
 
-	Type    *string
+	Type *string
 	Comment *string `json:",omitempty"`
+	Next *string
+	Branches []StateMachine
+}
+
+type BranchExecution struct {
+	Execution *Execution
+	ExecutionError error
+}
+
+type ParallelStateExecution struct {
+	BranchExecution []*BranchExecution
 }
 
 func (s *ParallelState) Execute(_ context.Context, input interface{}) (output interface{}, next *string, err error) {
-	return input, nil, nil
+	// parallel state according to asl spec does not assume Next therefore we could run each branch as a separate
+    execution := ParallelStateExecution{}
+    execution.BranchExecution = make([]*BranchExecution, len(s.Branches))
+    awaiter := sync.WaitGroup{}
+    awaiter.Add(len(s.Branches))
+	for i, b := range s.Branches {
+		go func(index int, branch *StateMachine){
+			result, err := branch.Execute(input)
+			execution.BranchExecution[index] = &BranchExecution{}
+			execution.BranchExecution[index].Execution = result
+			execution.BranchExecution[index].ExecutionError = err
+			defer awaiter.Done()
+		}(i, &b)
+	}
+	awaiter.Wait()
+	// TODO: UMV: do think how to use execution (ParallelStateExecution)
+	return nil, s.Next, nil
 }
 
 func (s *ParallelState) Validate() error {
